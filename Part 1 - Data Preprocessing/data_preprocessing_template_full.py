348654262cb599995858deca7fba407563fd124a
Wenlong <wenlongjiang@Wenlongs-iMac.local>
2018-01-29 01:04:53 -0500

    finish data preprocessing

--- Part 1 - Data Preprocessing/data_preprocessing_template_full.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template_full.py
@@ -15,3 +15,22 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
