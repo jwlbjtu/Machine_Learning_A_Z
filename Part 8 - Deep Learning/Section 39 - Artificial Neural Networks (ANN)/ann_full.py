8bd362694224ebb38d437ea8b1f07c2a23e45f2e
Wenlong <wenlongjiang@Wenlongs-iMac.cable.rcn.com>
2018-12-09 20:10:28 -0500

    fixed confusion metix

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_full.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_full.py
@@ -44,7 +44,7 @@ classifier = Sequential()
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
 
 # Adding the second hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+#classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
 
 # Adding the output layer
 classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
@@ -59,7 +59,8 @@ classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
-from sklearn.matrics import confusion_matrix
+from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
